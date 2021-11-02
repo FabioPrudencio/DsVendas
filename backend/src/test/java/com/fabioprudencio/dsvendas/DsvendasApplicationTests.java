cc93708759a15570c25999adeb8d8785c1fe75cd
Fabio Prudencio <fabio.oliveira.prudencio@gmail.com>
2021-11-02 15:06:29 -0300

    Project created

---
+++ backend/src/test/java/com/fabioprudencio/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabioprudencio.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
