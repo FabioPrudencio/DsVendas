dc12cb85199d59050421bb32562b5abcd3893d4b
Fabio Prudencio <fabio.oliveira.prudencio@gmail.com>
2021-11-06 20:35:03 -0300

    Group by search

--- backend/src/main/java/com/fabioprudencio/dsvendas/services/SaleService.java
+++ backend/src/main/java/com/fabioprudencio/dsvendas/services/SaleService.java
@@ -1,5 +1,7 @@
 package com.fabioprudencio.dsvendas.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.fabioprudencio.dsvendas.dto.SaleDTO;
+import com.fabioprudencio.dsvendas.dto.SaleSuccessDTO;
+import com.fabioprudencio.dsvendas.dto.SaleSumDTO;
 import com.fabioprudencio.dsvendas.entities.Sale;
 import com.fabioprudencio.dsvendas.repositories.SaleRepository;
 import com.fabioprudencio.dsvendas.repositories.SellerRepository;
@@ -26,4 +30,17 @@ public class SaleService {
 		Page<Sale> result = repository.findAll(pageable);
 		return result.map(x -> new SaleDTO(x));
 	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSumDTO> amountGroupedBySeller(){
+		return repository.amountGroupedBySeller();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSuccessDTO> successGroupedBySeller(){
+		return repository.successGroupedBySeller();
+	}	
+	
+	
+	
 }
